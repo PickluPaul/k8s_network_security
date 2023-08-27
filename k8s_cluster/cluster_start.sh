13ed83da1da2410f9c4a76f1bb63f96e3f4a031c
Picklu Paul <picklu.paul@ovo.id>
2023-08-27 12:55:18 +0800

    updating network policies to allow connection to virustotal

--- k8s_cluster/cluster_start.sh
+++ k8s_cluster/cluster_start.sh
@@ -1,8 +1,6 @@
-minikube start -p randomcluster  --network-plugin=cni --cni=calico \
+minikube start -p myapp  --network-plugin=cni --cni=calico \
     --extra-config=apiserver.audit-policy-file=/etc/ssl/certs/audit-policy.yaml \
     --extra-config=apiserver.audit-log-path=- \
     --extra-config=apiserver.audit-webhook-config-file=/etc/ssl/certs/webhook-config.yaml \
     --extra-config=apiserver.audit-webhook-batch-max-size=10 \
-    --extra-config=apiserver.audit-webhook-batch-max-wait=5s \
-    --cpus=4 \
-    --driver=hyperkit
+    --extra-config=apiserver.audit-webhook-batch-max-wait=5s 
